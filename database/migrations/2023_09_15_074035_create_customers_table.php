d3d0b5c836ca87c1cf2d9048b5c7e74daa6daf8c
Sazad Ahemad <mail2sazad@gmail.com>
2023-09-15 13:54:11 +0530

    added dashboard, customers page & table

---
+++ database/migrations/2023_09_15_074035_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('email')->unique();
+            $table->string('contact')->unique();
+            $table->string('addon_contact')->nullable();
+            $table->longText('address');
+            $table->string('city');
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
---
+++ resources/views/customers.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout');
+
+@section('main')
+    <div class="page-heading">
+        <h3>Customers</h3>
+    </div>
+    <div class="page-content">
+        <section class="row">
+            <div class="col-12 col-lg-9">
+                asdfdsa
+            </div>
+        </section>
+    </div>
+@endsection
